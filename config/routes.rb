34714ec44c2b1ffdeec1ba87a64515abaeabdb47
Jorge Aviles <javiles.dev@gmail.com>
2017-07-04 18:47:14 -0400

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+   
+
+  post "/custom_sign_up", to: "users/omniauth_callbacks#custom_sign_up"
+
+  authenticated :user do
+    root "home#index"
+  end
+
+  unauthenticated :user do
+    root "home#landing"
+  end
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
